250b19a47d1dab63d6a032f658df98973a85c8fa
Sim-karta <simonenkonikita561@gmail.com>
2026-08-02 13:05:07 +0300

    Add buttons for interacting with customer

--- assets/scripts/customer.js
+++ assets/scripts/customer.js
@@ -1,3 +1,11 @@
+const customerElement = document.querySelector(".customer");
+const printObjBtnEl = customerElement.querySelector(".print-obj-btn");
+const printAddressBtnEl = customerElement.querySelector(".print-address-btn");
+const changePhoneBtnEl = customerElement.querySelector(".change-phone-btn");
+const printGenderBtnEl = customerElement.querySelector(".print-gender-btn");
+const deleteAddressBtnEl = customerElement.querySelector(".delete-address-btn");
+const printCopiesBtnEl = customerElement.querySelector(".print-copies-btn");
+
 const customer = {
     name: "Нікіта",
     secondName: "Симоненко",
@@ -14,6 +22,7 @@ const customer = {
 
     printAddress() {
         console.log("Адреса:");
+        this.address ?? console.log("Не вказано");
         for (key in this.address) {
             console.log(`${key} - ${this.address[key]}`);
         }
@@ -28,12 +37,30 @@ customer.isMale = function () {
     return this.gender === "male" ? "male" : "female";
 };
 
-delete customer.address;
-
-console.log("Customer >> ", customer);
-
-const newCustomer = Object.assign(customer);
+const newCustomer = Object.assign({}, customer);
 const customer1 = { ...customer };
 
-console.log("newCustomer >> ", newCustomer);
-console.log("customer1 >> ", customer1);
+printObjBtnEl.addEventListener("click", () => {
+    console.log("Customer >> ", customer);
+});
+
+printAddressBtnEl.addEventListener("click", () => {
+    customer.printAddress();
+});
+
+changePhoneBtnEl.addEventListener("click", () => {
+    customer.changeNumber(prompt("Введіть новий номер"));
+});
+
+printGenderBtnEl.addEventListener("click", () => {
+    console.log(customer.isMale());
+});
+
+deleteAddressBtnEl.addEventListener("click", () => {
+    delete customer.address;
+});
+
+printCopiesBtnEl.addEventListener("click", () => {
+    console.log("newCustomer >> ", newCustomer);
+    console.log("customer1 >> ", customer1);
+});
