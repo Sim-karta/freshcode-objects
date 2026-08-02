94f1e9a82d16c62a3459cfd5eb3db81c3ba5444e
Sim-karta <simonenkonikita561@gmail.com>
2026-08-02 20:11:41 +0300

    Add prototype

--- assets/scripts/book.js
+++ assets/scripts/book.js
@@ -9,6 +9,20 @@ function Book(author, title, publishYear, publishCity, publishName, price) {
     this.price = price;
 }
 
+const bookMethods = {};
+
+bookMethods.calcAge = function () {
+    const currentYear = new Date().getFullYear();
+
+    return currentYear - this.publishYear;
+};
+
+bookMethods.changePrice = function (newPrice) {
+    this.price = newPrice;
+};
+
+Book.prototype = bookMethods;
+
 const book1 = new Book(
     "Джордж Мартін",
     "Пісня льоду й полум'я",
@@ -17,5 +31,11 @@ const book1 = new Book(
     "Країна Мрій",
     1200,
 );
+
+console.log(book1);
+
+console.log(book1.calcAge());
+
+book1.changePrice(1500);
 
 console.log(book1);
